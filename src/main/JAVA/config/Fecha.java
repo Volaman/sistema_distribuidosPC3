ffe38ce03a97a418a7a5ed96d619bb46b03ceebd
jefeson <jefersonalvarezmego@gmail.com>
2024-10-25 20:35:27 -0500

    CAMBIO DE FECHA

--- src/main/JAVA/config/Fecha.java
+++ src/main/JAVA/config/Fecha.java
@@ -17,7 +17,7 @@ public class Fecha {
         fecha = sdf.format(calendar.getTime());
         return fecha;
     }
-//adicion prueba
+//adicion prueba 1
     public static String FechaBD() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         fecha = sdf.format(calendar.getTime());
